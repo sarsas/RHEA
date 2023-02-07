2264ff69f5e6672faa4a634a91fdec923a9f10de
sarsas <sara.jahangirii@gmail.com>
2023-02-07 15:24:51 +0100

    Add project files.

---
+++ AccountInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHEA
+{
+    public class AccountInfo
+    {
+        private readonly int _accountId;
+        private readonly IAccountService _accountService;
+
+        public AccountInfo(int accountId, IAccountService accountService)
+        {
+            _accountId = accountId;
+            _accountService = accountService;
+        }
+
+        public double Amount { get; private set; }
+
+        public async Task RefreshAmountAsync()
+        {
+            Amount = await _accountService.GetAccountAmountAsync(_accountId);
+        }
+
+    }
+}
---
+++ IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHEA
+{
+    public interface IAccountService
+    {
+        Task<double> GetAccountAmountAsync(int accountId);
+    }
+}
