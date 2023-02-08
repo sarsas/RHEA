b6d30fc9ca314c2b7c631c583e6b689d9940ce47
sarsas <sara.jahangirii@gmail.com>
2023-02-08 11:53:28 +0100

    test unit

---
+++ TestRHEA/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestRHEA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestRHEA")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5c13b44-45d9-440b-b512-91fb6bba41b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestRHEA/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RHEA;
+using Moq;
+using System.Threading.Tasks;
+
+namespace TestRHEA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            // Arrange
+            int accountId = 1;
+            double expectedAmount = 1000.0;
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.GetAccountAmountAsync(accountId))
+                .ReturnsAsync(expectedAmount);
+
+            var accountInfo = new AccountInfo(accountId, accountServiceMock.Object);
+
+            await accountInfo.RefreshAmountAsync();
+
+            Assert.AreEqual(expectedAmount, accountInfo.Amount);
+        }
+    }
+}
+
